7f91bfde2b10cd6fd9b07810294898d4ea24b39d
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-05-31 07:20:30 +0530

    Project Initialized and User Class Created

---
+++ src/main/java/User.java
@@ -0,0 +1,58 @@
+public class User {
+    private String userId;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+
+    //User ID
+    public void setUserId(String userId) {
+        userId = this.userId;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    //Name
+    public void setName(String name) {
+        name = this.name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    //Address
+    public void setAddress(String address) {
+        address = this.address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    //Phone Number
+    public void setPhoneNumber(String phoneNumber) {
+        phoneNumber = this.phoneNumber;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    //Email
+    public void setEmail(){
+        email = this.email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    //toString
+    public String toString() {
+        return ("User ID: " + this.getUserId() + "Name: " + this.getName() + "Address: " + this.getAddress() + "Phone Number: " + this.getPhoneNumber() + "Email: " + this.getEmail());
+    }
+}
